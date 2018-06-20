82e51f7370d86acd5d47212de7e4e1a685e91fdf
Tkdefender <tkdefender88@gmail.com>
2018-06-20 15:37:49 -0600

    made a route to add dsms

--- Express/routes/dsm.js
+++ Express/routes/dsm.js
@@ -1,11 +1,23 @@
 // This route is for defining dsm requests and stuff
+
+//requires
 const express = require('express'),
         mongo = require('mongodb'),
         MongoClient = mongo.MongoClient,
-        mongoURL = 'mongodb://localhost:27017',
-        dbName = 'ds';
+		fs = require('fs'),
+		bodyParser = require('body-parser')
+		path = require('path')
+		assert = require('assert')
+
 
 let router = express.Router()
+
+// constants
+const mongoURL = 'mongodb://localhost:27017',
+	dbName = 'ds',
+	dsmColName = 'dsms'
+
+router.use(bodyParser)
 
 //route returns a dsm of a particular id
 router.route('/dsm/:id')
@@ -34,9 +46,11 @@ router.route('/dsm/:id')
 		.catch(queryErr) //I broke my promise guys, I'm sorry
 	})
 })
+
 .all((req, res) => {
 	res.status(405)
 })
+
 
 //a route that returns a list of all the dsms in the database
 router.route('/dsm')
@@ -73,6 +87,36 @@ router.route('/dsm')
 		})
 	})	
 })
+.post((req, res) => {
+	let body = req.body;
+	
+	MongoClient.connect(mongoURL, {useNewUrlParser: true},  (err, client) => {
+		if (err) {
+			console.error(err)
+
+			res.status(500)
+
+			return
+		}
+
+		const db = client.db(dbName)
+
+		let dsm = JSON.parse(body) 
+
+		let dsmObj = new DSM(dsm._id, dsm.title, dsm.description,
+			dsm.version, dsm.author, dsm.config,
+			dsm.stylesheet, dsm.style, dsm.contributors,
+			dsm.idle_backgrounds, dsm.video_select_backgrounds,
+			dsm.ce_set, dsm.attributes)
+		
+		return add(db, dsmColName, dsmObj)
+		.then((res, result) => {
+			console.log(`Inserted a dsm with _id: ${result.insertedID}`)
+			res.status(200)
+			client.close()
+		})
+	})
+})
 .all((req, res) => {
 	res.status(405);
 })
@@ -93,6 +137,14 @@ function query(db, collectionName, id) {
 
 function queryErr(err) {
 	console.error(err)
+}
+
+function add(db, collectionName, obj, opts = { }) {
+	return new Promise((resolve, reject) => {
+		let collection = db.collection(collectionName)
+
+		collection.insertOne(obj, opts).then(resolve).catch(reject);
+	})
 }
 
 module.exports = router
