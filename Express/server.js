cf6ab8242575613f72d5c16e19de30c1fc1560b0
Tkdefender <tkdefender88@gmail.com>
2018-06-20 12:17:30 -0600

    added support for CORS

--- Express/server.js
+++ Express/server.js
@@ -24,6 +24,19 @@ const port = 30120;
 const static_dir = '../WebApp/dist';
 const static_path = path.join(__dirname, static_dir);
 
+var allowCrossDomain = function(req, res, next) {
+    if ('OPTIONS' == req.method) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        res.send(200);
+    } else {
+        next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 //-------------------
 //- Server Listener -
 //-------------------
