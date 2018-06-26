286e48d7e26ca7c4a093e07e86f733ab71f662bf
Michael Fryer <michael.d.fryer@gmail.com>
2018-06-26 09:37:05 -0600

    adjusted the cors policy to allow all cross-origin get requests
    	modified:   server.js

--- Express/server.js
+++ Express/server.js
@@ -24,18 +24,15 @@ const port = 30120;
 const static_dir = '../WebApp/dist';
 const static_path = path.join(__dirname, static_dir);
 
-var allowCrossDomain = function(req, res, next) {
-    if ('OPTIONS' == req.method) {
+var allowCrossDomainGet = function(req, res, next) {
+    if ('GET' === req.method) {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-        res.send(200);
-    } else {
-        next();
     }
+
+    next();
 };
 
-app.use(allowCrossDomain);
+app.use(allowCrossDomainGet);
 
 //-------------------
 //- Server Listener -
