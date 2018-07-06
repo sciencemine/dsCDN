26c8d98930b9e03ace83aa18c7ba75663352d8aa
TKDefender <tkdefender88@gmail.com>
2018-07-06 13:13:40 -0600

    added a file for the util functions to reduce repeated code

---
+++ Express/util.js
@@ -0,0 +1,29 @@
+//A set of untility functions to reduce code repition
+
+function query(db, collectionName, id) {
+	return new Promise((resolve, reject) => {
+		let collection = db.collection(collectionName)
+
+		collection.find({_id: id }).toArray((err, doc) => {
+			if (err) {
+				reject(err)
+			}
+
+			resolve(doc[0])
+		})
+	})
+}
+
+function queryErr(err) {
+	console.error(err)
+}
+
+function add(db, collectionName, obj, opts = { }) {
+	return new Promise((resolve, reject) => {
+		let collection = db.collection(collectionName)
+
+		collection.insertOne(obj, opts).then(resolve).catch((err) => { console.log(err); reject(err); })
+	})
+}
+
+module.exports = add, query, queryErr
